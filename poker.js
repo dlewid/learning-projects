dba54474acbb0719b1ef863fe0edf738d7f8330e
Daulton Widlacki <soldbuydaulton@gmail.com>
2025-07-23 21:11:43 -0500

    Added a card class, with some functionality

--- poker.js
+++ poker.js
@@ -1,30 +1,73 @@
 //Created by Daulton Widlacki. Create a command line poker game to brush up on JS.
 
-
-// TO-DO: 
+// TO-DO:
 /* 
 - Create a class for player, ops, deck.
 - Create main function loop
 */
 
-
 // This is a class declaration in JS ```class ClassName```
 class Player {
-    constructor () {
+  constructor(starting_dough) {
+    // instance variables assignment using keyword this
+    this.player_name = "Player 1";
+    this.cash = starting_dough;
+    this.hand = [new Card(), new Card()];
+  }
 
-    }
+  // Creating a magic method... no need for function keyword
+  toString() {
+    return "Name: " + this.player_name + "\nCash: " + this.cash;
+  }
 }
 
 class Op {
-
+  constructor() {}
 }
 
 class Deck {
-
+  constructor() {}
 }
 
 class River {
-
+  constructor() {}
 }
 
-console.log("Hello, World!")
+// why declare a class like this compared to class keyword?
+const Card = class {
+  constructor() {
+    this.suit = this.get_card_suit();
+    this.value = this.get_card_value();
+  }
+
+  // return suit
+  get_card_suit() {
+    return Math.floor(Math.random() * 4) + 1;
+  }
+
+  get_card_value() {
+    return Math.floor(Math.random() * 13) + 1;
+  }
+
+  // test to see if Math.floor(Math.random() * max_value) was not a logical error
+  test() {
+    for (let i = 0; i < 100; i++) {
+      console.log(
+        "Suit: " + this.get_card_suit() + " Value: " + this.get_card_value()
+      );
+    }
+  }
+
+  get_suit() {
+    let suits = { 1: "clubs", 2: "hearts" };
+  }
+
+  toString() {}
+};
+
+//console.log("Hello, World!") printing to console
+// let is a local
+// var is function scoped: when var is used in main block then it is global
+
+let player = new Player();
+consol;
--- write_up.md
+++ write_up.md
@@ -1,1 +1,6 @@
 ## Wanted to see how everything looked
+
+# day 1 July 23/2025
+- Brushed up on pushing to github
+- learned basic syntax like console log, class, constructor declaration, parameter passing fundementals (dynamically typed)
+-
